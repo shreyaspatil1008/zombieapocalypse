5bd4216ce606cde480a854dc566f784ff05bbc64
ankittech <ankitech@gmail.com>
2020-05-03 00:07:43 +0530

    added nopopPasswordEncoder

--- src/main/java/com/shreyas/zombieapocalypse/config/SpringSecurityConfig.java
+++ src/main/java/com/shreyas/zombieapocalypse/config/SpringSecurityConfig.java
@@ -1,14 +1,15 @@
 package com.shreyas.zombieapocalypse.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -28,5 +29,11 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("ailo").password("password").roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        //TODO: enable encode
+        return NoOpPasswordEncoder.getInstance();
     }
 }
