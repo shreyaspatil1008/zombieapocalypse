8ba639428f5dd465539fdd334e0f036f3171599d
ankittech <ankitech@gmail.com>
2020-05-02 23:59:03 +0530

    refactored controller advise

--- src/main/java/com/shreyas/zombieapocalypse/controller/ZombieApocalypseController.java
+++ src/main/java/com/shreyas/zombieapocalypse/controller/ZombieApocalypseController.java
@@ -4,14 +4,11 @@ import com.shreyas.zombieapocalypse.model.InputRequest;
 import com.shreyas.zombieapocalypse.model.OutputResponse;
 import com.shreyas.zombieapocalypse.service.ZombieMovementService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class ZombieApocalypseController {
@@ -24,18 +21,5 @@ public class ZombieApocalypseController {
     public OutputResponse apocalypse(@Valid @RequestBody InputRequest inputRequest){
         zombieMovementService.init(inputRequest);
         return zombieMovementService.move();
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
     }
 }
