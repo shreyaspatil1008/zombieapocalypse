8fe3b573412d5172373316163facce383264557b
Shreyu <shreyaspatil1008@gmail.com>
2020-05-03 04:32:29 +1000

    Code refactoring

---
+++ src/main/java/com/shreyas/zombieapocalypse/config/WebMvcConfigurer.java
@@ -0,0 +1,5 @@
+package com.shreyas.zombieapocalypse.config;
+
+import org.springframework.context.annotation.Bean;
+
+
--- src/main/java/com/shreyas/zombieapocalypse/controller/advice/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/shreyas/zombieapocalypse/controller/advice/RestResponseEntityExceptionHandler.java
@@ -1,12 +1,12 @@
 package com.shreyas.zombieapocalypse.controller.advice;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+        import org.springframework.http.HttpHeaders;
+        import org.springframework.http.HttpStatus;
+        import org.springframework.http.ResponseEntity;
+        import org.springframework.web.bind.annotation.ControllerAdvice;
+        import org.springframework.web.bind.annotation.ExceptionHandler;
+        import org.springframework.web.context.request.WebRequest;
+        import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler
