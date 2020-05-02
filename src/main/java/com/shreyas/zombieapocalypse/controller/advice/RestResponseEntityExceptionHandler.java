8ae7358d7e6386c6e0b6cdd7fbceef6a29d0d465
Shreyu <shreyaspatil1008@gmail.com>
2020-05-03 04:34:19 +1000

    Code refactoring

--- src/main/java/com/shreyas/zombieapocalypse/controller/advice/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/shreyas/zombieapocalypse/controller/advice/RestResponseEntityExceptionHandler.java
@@ -18,7 +18,10 @@ public class RestResponseEntityExceptionHandler
         extends ResponseEntityExceptionHandler {
 
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
