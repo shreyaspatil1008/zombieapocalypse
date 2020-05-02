0ad5d82b4b07172ab4f3d08f773fb72d2f45e0d8
Shreyu <shreyaspatil1008@gmail.com>
2020-05-03 04:09:19 +1000

    Code refactoring

--- src/main/java/com/shreyas/zombieapocalypse/controller/advice/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/shreyas/zombieapocalypse/controller/advice/RestResponseEntityExceptionHandler.java
@@ -12,8 +12,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class RestResponseEntityExceptionHandler
         extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value
-            = { IllegalArgumentException.class, IllegalStateException.class })
+    @ExceptionHandler(Exception.class)
     protected ResponseEntity<Object> handleConflict(
             RuntimeException ex, WebRequest request) {
 
