da6b07b337a1a0c949209ad6b0527fd300d1b59a
Shreyu <shreyaspatil1008@gmail.com>
2020-05-03 14:14:49 +1000

    Test cases

--- src/test/java/com/shreyas/zombieapocalypse/model/WorldTest.java
+++ src/test/java/com/shreyas/zombieapocalypse/model/WorldTest.java
@@ -1,28 +1,45 @@
 package com.shreyas.zombieapocalypse.model;
 
+import com.shreyas.zombieapocalypse.enums.Direction;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 class WorldTest {
 
     private World world;
+    private List<Creature> creatures;
+    private Creature zombie;
 
-    @Test
-    void zombieToCreatureBiteTest() {
+    @BeforeAll
+    public void setUp() {
         Creature creature = new Creature(new Position(1, 1), false);
-        Creature zombie = new Creature(new Position(1, 1), true);
-        Creature returnedCreature = zombie.bites(creature);
-        Creature expectedCreature = new Creature(new Position(1, 1), true);
-        Assertions.assertEquals(returnedCreature, expectedCreature);
+        Creature anotherCreature = new Creature(new Position(1, 1), false);
+        creatures = new ArrayList<>();
+        creatures.add(creature);
+        creatures.add(anotherCreature);
+        zombie = new Creature(new Position(1, 1), true);
+        world = new World(4, zombie, creatures);
     }
 
     @Test
-    void zombieToZombieBiteTest() {
-        Creature creature = new Creature(new Position(1, 1), true);
-        Creature zombie = new Creature(new Position(1, 1), true);
-        Creature returnedCreature = zombie.bites(creature);
-        Creature expectedCreature = new Creature(new Position(1, 1), true);
-        Assertions.assertEquals(returnedCreature, expectedCreature);
+    void moveZombieTestWithOptionalValue() {
+        Optional<Creature> optionalCreature = world.moveZombie(Direction.D);
+        Assertions.assertEquals(optionalCreature.isPresent(), true);
+        //Assertions.assertEquals(optionalCreature.get(), anotherCreature);
     }
+
+    @Test
+    void moveZombieTestWithOptionalEmpty() {
+        Optional<Creature> optionalCreature = world.moveZombie(Direction.D);
+        Assertions.assertEquals(optionalCreature.isPresent(), true);
+        //Assertions.assertEquals(optionalCreature.get(), anotherCreature);
+    }
+
+
 
 }
